b1962b4a2024d9657d3fbeeb37c7618135c814a0
Galib-23 <aagalib2323@gmail.com>
2024-09-28 23:48:24 +0600

    complete yt carousel

--- src/Pages/Home/youtube/Youtube.jsx
+++ src/Pages/Home/youtube/Youtube.jsx
@@ -1,16 +1,18 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import YoutubeCard from "./YoutubeCard";
-import imgThumb from "../../../assets/flavor.png";
 import discordThumb from '../../../assets/disthumb.png'
+import formifyThumb from '../../../assets/formifythumb.png'
+import promptthumb from '../../../assets/promptthumb.png'
+import circlespacethumb from '../../../assets/circlespacethumb.png'
 
 
 const Youtube = () => {
   const customThumbnails = [
     discordThumb,
-    imgThumb,
-    imgThumb,
-    imgThumb,
+    formifyThumb,
+    promptthumb,
+    circlespacethumb,
   ];
 
   return (
@@ -34,21 +36,21 @@ const Youtube = () => {
         <div>
           <YoutubeCard
             srcVid={
-              "https://www.youtube.com/embed/RusKXEW1Lb8?si=3uFH2yjQpd3QxI_U"
+              "https://www.youtube.com/embed/11yGMX6gqqY?si=buNLJQdsGM3gfMar"
             }
           />
         </div>
         <div>
           <YoutubeCard
             srcVid={
-              "https://www.youtube.com/embed/RusKXEW1Lb8?si=3uFH2yjQpd3QxI_U"
+              "https://www.youtube.com/embed/c-09QAca8OM?si=CvbFVC22P20fimbg"
             }
           />
         </div>
         <div>
           <YoutubeCard
             srcVid={
-              "https://www.youtube.com/embed/RusKXEW1Lb8?si=3uFH2yjQpd3QxI_U"
+              "https://www.youtube.com/embed/gHiwKF-sZ8k?si=0zvWf0FoJ6oqy1Wa"
             }
           />
         </div>
